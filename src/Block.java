8f804c0b3519d82b12284f0027baa95d3e36cb7f
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-05 20:05:50 -0300

    Mining blocks

--- src/Block.java
+++ src/Block.java
@@ -6,6 +6,7 @@ public class Block {
     public String previousHash;
     private String data; //our data will be a simple message.
     private long timeStamp; //as number of milliseconds since 1/1/1970
+    private int nonce;
 
     //Block Constructor
     public Block(String data, String previousHash) {
@@ -19,8 +20,18 @@ public class Block {
         String calculatedHash = StringUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp) +
+                        Integer.toString(nonce) +
                         data
         );
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
+        while (!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
     }
 }
