b8df9f4b4c5931f0a35c6be9c436c3b5938522e8
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-05 18:52:40 -0300

    Creating blockchain and using Gson library

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -1,15 +1,18 @@
+import com.google.gson.GsonBuilder;
+
 import java.util.ArrayList;
 
 public class NoobChain {
 
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+
     public static void main(String[] args) {
-        Block genesisBlock = new Block("Hi im the first block", "0");
-        System.out.println("Hash for block 1 : " + genesisBlock.hash);
+        //add our blocks to the blockchain Arraylist:
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).hash));
+        blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).hash));
 
-        Block secondBlock = new Block("Yo im the second block", genesisBlock.hash);
-        System.out.println("Hash for block 2 : " + secondBlock.hash);
-
-        Block thirdBlock = new Block("Hey im the third block", secondBlock.hash);
-        System.out.println("Hash for block 3 : " + thirdBlock.hash);
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
     }
 }
