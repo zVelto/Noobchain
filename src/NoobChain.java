1c195500d1220fc788eb73f82ff0f581a0c5686e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-05 19:27:39 -0300

    Checking integrity of the blockchain

--- src/NoobChain.java
+++ src/NoobChain.java
@@ -15,4 +15,26 @@ public class NoobChain {
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
     }
+
+    public static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+
+        //loop through blockchain to check hashes:
+        for (int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash())) {
+                System.out.println("Current hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash)) {
+                System.out.println("Previous hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
 }
