ee5a050fabff54700cd124588cc60c06e11d8e6e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-02-06 18:33:15 -0300

    GenerateKeyPair method

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,8 +1,28 @@
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
 
 public class Wallet {
 
     public PrivateKey privateKey;
     public PublicKey publicKey;
+
+    public Wallet() {
+        generateKeyPair();
+    }
+
+    public void generateKeyPair() {
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
+            //Initialize the key generator and generate a KeyPair
+            keyGen.initialize(ecSpec, random);  //256 bytes providers an acceptable security
+            KeyPair keyPair = keyGen.generateKeyPair();
+            //Set the public keys from the keyPair
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
